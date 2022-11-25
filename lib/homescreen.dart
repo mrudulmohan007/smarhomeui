f236919b0db7872cc686ae3c7e2ac836dabe0f4e
mrudulmohan007 <mrudulmohan2001@gmail.com>
2022-11-26 01:26:22 +0530

    third commit

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -22,6 +22,49 @@ class _HomeScreenState extends State<HomeScreen> {
                 CupertinoIcons.list_dash,
                 color: Colors.grey.shade500,
               )),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  'images/smarthome.png',
+                ),
+                radius: 18,
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'My Smart Home',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                              shadows: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  blurRadius: 3,
+                                  offset: Offset(2, 3),
+                                )
+                              ]),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
